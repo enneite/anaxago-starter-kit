457dcbda761b6dc23e63f38acce82ecc214adcfb
Etienne LE JARIEL <etienne.lejariel@PLI-5CD651342Y.wonderbox.lan>
2019-03-20 01:16:46 +0100

    correctif dans le swagger

---
+++ src/Anaxago/CoreBundle/Controller/Api/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: etienne.lejariel
+ * Date: 20/03/19
+ * Time: 00:46
+ */
+
+namespace Anaxago\CoreBundle\Controller\Api;
+
+
+class ApiController
+{
+
+}
---
+++ src/Anaxago/CoreBundle/Controller/Api/ProposalController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: etienne.lejariel
+ * Date: 20/03/19
+ * Time: 00:44
+ */
+
+namespace Anaxago\CoreBundle\Controller\Api;
+
+
+class ProposalController
+{
+
+}
