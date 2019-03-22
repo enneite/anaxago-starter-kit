093e8ef87ac4a47341752715a865e08b2e982726
Etienne LE JARIEL <etienne.lejariel@PLI-5CD651342Y.wonderbox.lan>
2019-03-22 18:22:59 +0100

    Ajout de statut pour les projets en base de données

---
+++ app/DoctrineMigrations/Version20190322155349.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190322155349 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project ADD status VARCHAR(10) DEFAULT \'OPEN\' NOT NULL, ADD amount NUMERIC(10, 2) NOT NULL, ADD reached TINYINT(1) DEFAULT \'0\', ADD currency VARCHAR(3) DEFAULT \'EUR\' NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project DROP status, DROP amount, DROP reached, DROP currency');
+    }
+}
--- src/Anaxago/CoreBundle/Entity/Project.php
+++ src/Anaxago/CoreBundle/Entity/Project.php
@@ -4,6 +4,7 @@ namespace Anaxago\CoreBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Project
@@ -42,6 +43,44 @@ class Project
      * @ORM\Column(name="description", type="text")
      */
     private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=10, options= {"default" = "OPEN"})
+     *
+     * @Assert\NotBlank()
+     * @Assert\Choice(choices = {"OPEN", "FUNDED"}, message = "unvalid status for project")
+     */
+    private $status;
+
+    /**
+     * Montant du projet
+     *
+     * @var float
+     *
+     * @ORM\Column(name="amount", type="decimal", precision=10, scale=2)
+     */
+    private $amount;
+
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="reached", type="boolean", options= {"default" = false}, nullable=true)
+     */
+    private $reached;
+
+
+    /**
+     * code ISO de la devise
+     *
+     * @var string
+     *
+     * @ORM\Column(name="currency", type="string", length=3, options= {"default" = "EUR"})
+     *
+     */
+    private $currency;
 
     /**
      * @var ArrayCollection
@@ -137,6 +176,72 @@ class Project
     {
         return $this->description;
     }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param float $amount
+     */
+    public function setAmount(float $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReached(): bool
+    {
+        return $this->reached;
+    }
+
+    /**
+     * @param bool $reached
+     */
+    public function setReached(bool $reached): void
+    {
+        $this->reached = $reached;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency
+     */
+    public function setCurrency(string $currency): void
+    {
+        $this->currency = $currency;
+    }
+
+
 
 
     /**
