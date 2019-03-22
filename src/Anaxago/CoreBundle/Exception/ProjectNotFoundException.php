e7fb282c280b6ddcb33296225d1d44e823cf1fe3
Etienne LE JARIEL <etienne.lejariel@PLI-5CD651342Y.wonderbox.lan>
2019-03-22 02:01:21 +0100

    Creation de proposition de financement, commit incomplet

---
+++ src/Anaxago/CoreBundle/Exception/ProjectNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: etienne.lejariel
+ * Date: 22/03/19
+ * Time: 01:00
+ */
+
+namespace Anaxago\CoreBundle\Exception;
+
+
+class ProjectNotFoundException extends \Exception
+{
+
+}
